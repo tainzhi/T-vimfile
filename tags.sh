91cddb606eaedd8deb73264b79a4f052faf178fa
tainzhi <qfq61@qq.com>
2015-03-06 13:09:47 +0800

    add plugin ack.vim and change NEDRTree and tagbar place

--- tags.sh
+++ tags.sh
@@ -1,6 +1,9 @@
-find -iname "*.java" > ./cscope.files
-find -iname "*.xml" >> ./cscope.files
-#find . -name "*.h" -o -name "*.c" -o -name "*.cc" > cscope.files
+#find /home/qiufuqing/framework/1410W_EX/frameworks/base/core -iname "*.java" -o -name "*.xml" -o -name "*.h"  -o -name "*.c" -o -name "*.cpp" -o -name "*.cc" > ./cscope.files
+
+find /home/qiufuqing/framework/1420F_EX/LNX.LA.3.7.2-01110-8x16.0/frameworks -iname "*.java" -o -name "*.xml" -o -name "*.h"  -o -name "*.c" -o -name "*.cpp" -o -name "*.cc" > ./cscope.files
+
+#find -iname "*.xml" >> ./cscope.files
+#find . -name "*.h" -o -name "*.c" -o -name "*.cc" -o -name "*.cpp" > cscope.files
 
 ctags --fields=+i -n -R -L ./cscope.files
 
