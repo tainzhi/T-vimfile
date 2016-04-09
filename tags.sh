ef151d2d73d054c1c9ddab63c7eb94a0672e61bb
tainzhi <qfq61@qq.com>
2016-04-09 23:50:57 +0800

    delete tags.sh

--- tags.sh
+++
@@ -1,9 +0,0 @@
-CURRENT_PATH=`pwd`
-find $CURRENT_PATH -iname "*.java" -o -name "*.xml" -o -name "*.h"  -o -name "*.c" -o -name "*.cpp" -o -name "*.cc" > ./cscope.files
-
-#find -iname "*.xml" >> ./cscope.files
-#find . -name "*.h" -o -name "*.c" -o -name "*.cc" -o -name "*.cpp" > cscope.files
-
-ctags --fields=+i -n -R -L ./cscope.files
-
-cscope -bkq -i ./cscope.files
