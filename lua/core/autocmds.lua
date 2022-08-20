1715bc3928726f5cf9c0c9716acbce3de9fe445a
tainzhi <qfq61@qq.com>
2022-08-20 15:13:38 +0800

    add commit for firenvim

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -23,5 +23,7 @@ vim.cmd [[ autocmd BufWritePre *.md if &ft == 'markdown' | PanguAll]]
 vim.cmd [[ autocmd BufWinEnter *.md if &ft == 'markdown' | set shada="NONE"]]
 
 -- for chrome plugin firenvim
+-- https://github.com/glacambre/firenvim/issues/800
+-- 暂时不会把vim script转为 lua, 先这样实现
 vim.cmd [[ au BufEnter idart.mot.com_*.txt set filetype=idart lines=30 ]]
 vim.cmd [[ au BufEnter github.com_*.txt set filetype=markdown lines=30 ]]
