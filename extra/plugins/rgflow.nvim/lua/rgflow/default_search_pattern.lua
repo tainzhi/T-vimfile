f50a290a13a767269a743fc48de9337ac9b1aa46
tainzhi <qfq61@qq.com>
2025-05-08 11:15:30 +0800

    rgflow: add more fatal regex for default search pattern

--- extra/plugins/rgflow.nvim/lua/rgflow/default_search_pattern.lua
+++ extra/plugins/rgflow.nvim/lua/rgflow/default_search_pattern.lua
@@ -53,8 +53,13 @@ local default_patterns = {
     --
     "冻屏_freeze|lowmemorykiller.*cpuload ([456789]\\d|100)|lowmemorykiller.*cpu psi [456789]\\d|mempsi \\d\\d|low memory|cpuload ([789]\\d|100)|CPU usage.*\\d\\d\\d\\dms|CameraKpiTag.*\\d\\d\\d\\d ms|ActivityManager.*\\d\\d\\d\\dms|Mcflkp  : Alg benchmark.*process: \\d{4,} ms|McfCaptureRequestRunnable: Capture time:(\\d{5,}|[3456789]\\d{3})|FocusExposureLockRunnable:.*lock focus timeout|memory is very low",
     --AF timeout导致无法拍照 https://idart.mot.com/browse/IKSWV-94659
-    "CameraFsm| Fsm |MotoCamera: |CameraKpiTag| ActivityBase:|SHUTTER_BUTTON_CLICKED|playCaptureLottieAnimate shutterState:",
-    "engine错误|E CameraFsm|E CameraDevice|W CameraDevice|E Camera3-Device|E CamX : [ERROR]|E CamX.*Buffer|E CamX.*TimedWait|E CamX.*not|CAM_ERR.*Unexpected state|CamX.*error|CamX.*Failure|CamX.*failed|CamxResultETimeout|E CHI|E CHI.*bad state|CAM_ERR|there might be a leak|failed to get buffer|Unable to.*buffer|E MtkCam|CameraService|CameraService_proxy|Camera3-Device",
+    "CameraFsm| Fsm |MotoCamera: |CameraKpiTag| ActivityBase:|CameraModeSwitch|CameraStateManager|CloseAppRunnable|CloseCameraRunnable|CloseCameraRunnable|OpenCameraRunnable|OpenCameraCallable|SHUTTER_BUTTON_CLICKED|playCaptureLottieAnimate shutterState:",
+    -- 情况1: https://idart.mot.com/browse/IKSWV-158874
+    -- 06-04 08:10:11.857  1774 23657 W CallbackCore: [ACbAdptor-2-FrameHandler::updateMetaItem] [frameNumber 105] already receive error buffer, ignore meta
+    -- 06-04 08:10:11.857  1774 23657 I mtkcam-cb: [notifyErrors] input number=105 id=-1 error=3
+    -- 06-04 08:10:11.857  1774 23657 I mtkcam-cb: [prepareCallbackErrors] request 105 pending error result
+    -- 06-04 08:10:11.857  1774 23657 I CallbackCore: [ACbAdptor-2-FrameHandler::prepareErrorMetaIfPossible] [frameNumber 105], result error
+    "engine错误|onCaptureFailed|E CameraFsm|E CameraDevice|W CameraDevice|E Camera3-Device|E CamX : [ERROR]|E CamX.*Buffer|E CamX.*TimedWait|E CamX.*not|CAM_ERR.*Unexpected state|CamX.*error|CamX.*Failure|CamX.*failed|CamxResultETimeout|E CHI|E CHI.*bad state|CAM_ERR|there might be a leak|failed to get buffer|Unable to.*buffer|E MtkCam|CameraService|CameraService_proxy|Camera3-Device|receive error buffer|notifyErrors|prepareCallbackErrors",
     "ANR at|anr traces|Input dispatching timed out.*camera[35]|blocked by|held by thread|waiting to lock|I am_anr.*camera|begin ANR dump all threads|ActivityThread: main stack element",
     "系统重启|reboot|bootstat:|bootstat:.*kernel_panel",
     "模式和前后镜头切换|logicalCameraId is|logicalCameraId=|setupForMode",
